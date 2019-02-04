84c68e8b442b877cbf195c1db5df416491fc95fa
Andrzej Klajnert <github@aklajnert.pl>
2019-02-04 16:56:45 +0100

    Prepared test for nested object including query.

--- src/tests/test_nested.py
+++ src/tests/test_nested.py
@@ -1,7 +1,9 @@
 """Test example with nesting."""
+from collections import OrderedDict
+
 import graphene
 
-from .test_objects.nested import TopLevel
+from .test_objects.nested import TopLevel, Leaf
 from ..graphanno import graph_annotations
 
 
@@ -11,6 +13,22 @@ class NestedSchema:
     __model__ = TopLevel
 
 
+class NestedQuery(graphene.ObjectType):
+    """Query using nested class."""
+    top_level = graphene.Field(NestedSchema)
+
+    @staticmethod
+    def resolve_top_level(*_):
+        """Return the top level object instance"""
+        # pylint: disable=attribute-defined-outside-init
+        data = TopLevel()
+        leaf = Leaf()
+        leaf.value = 'some leaf value'
+        data.leaf = leaf
+        data.name = 'top level name'
+        return data
+
+
 def test_nested():
     """Validate result of the nested class resolving."""
     # pylint: disable=no-member
@@ -18,3 +36,14 @@ def test_nested():
     assert isinstance(NestedSchema.name, graphene.String)
     assert isinstance(NestedSchema.leaf, graphene.Field)
     assert isinstance(NestedSchema.leaf.type.value, graphene.String)
+
+
+def test_nested_query():
+    """Test the nested class behavior as a query."""
+    schema = graphene.Schema(query=NestedQuery)
+    response = schema.execute('{topLevel {name, leaf {value} } }')
+    assert response.data == OrderedDict([
+        ('topLevel', OrderedDict(
+            [('name', 'top level name'),
+             ('leaf', OrderedDict([
+                 ('value', 'some leaf value')]))]))])
